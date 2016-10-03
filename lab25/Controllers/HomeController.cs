764aea00b9d91fdac09e630b7ce17966f9310f85
phill720 <phillipschris902@gmail.com>
2016-10-03 16:48:28 -0400

    User Information Validation

--- lab25/Controllers/HomeController.cs
+++ lab25/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using lab25.Models;
 
 namespace lab25.Controllers
 {
@@ -13,18 +14,25 @@ namespace lab25.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(UserInfo userInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks",userInfo);
+            }
+            else
+            {
+                ViewBag.InvMessage = "Invalid Fields";
+                return View();
+            }
         }
     }
 }
---
+++ lab25/Models/UserInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace lab25.Models
+{
+    public class UserInfo
+    {
+        [Required]
+        [StringLength(20, ErrorMessage = "Your name must be 1 - 20 characters long.", MinimumLength = 1)]
+        public string Name { get; set; }
+        [Required]
+        [Range(1,150,ErrorMessage = "Age must be 1-150")]
+        public int Age { get; set; }
+    }
+}
